c7ee163f96dccb2df892489a72c011fa0b9bce53
Alejandro Mantilla <alejandromantilla7@hotmail.com>
2017-04-06 18:42:03 -0500

    gitignore and reference error fixed

--- scripts/download-electron.js
+++ scripts/download-electron.js
@@ -9,7 +9,9 @@ const ora = require('ora');
 
 let spinner = ora({text: 'Downloading Electron', stream: process.stdout}).start();
 
-const ELECTRON_URL = 'https://dl.dropboxusercontent.com/u/71884/Electron%201.4.12%20with%20window%20level%20fix.zip';
+// TODO: Report error with dropbox url (404 not found).
+//const ELECTRON_URL = 'https://dl.dropboxusercontent.com/u/71884/Electron%201.4.12%20with%20window%20level%20fix.zip';
+const ELECTRON_URL = 'https://github.com/electron/electron/releases/download/v1.6.2/electron-v1.6.2-darwin-x64.zip';
 const VENDOR_PATH = ['..', 'app', 'vendor'];
 
 const joinPath = (...str) => path.join(__dirname, ...str);
